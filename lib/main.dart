757f4089d162f1664a3bc7a4a9c2681b7a644eef
abdul <abdul.tanveer@gmail.com>
2024-07-01 11:17:11 +0530

    stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    Center(
-      child: Text("hello world",
-        textDirection: TextDirection.ltr,
-      style: TextStyle(fontSize: 25.0,color: Colors.green),),
-    )
+  MaterialApp(
+    title: "Demo-flutter-siemens" ,
+    home: MyWidget(),
+  )
   );
 }
+
+//stateless -- fruti tetra pack without juice, stateful -- tetra pack with juice -- juice = state
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const   Center(
+      child: Text("hello world",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(fontSize: 25.0,color: Colors.green),),
+    );
+  }
+}
+
+
+
