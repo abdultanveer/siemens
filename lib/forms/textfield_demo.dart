5d49ab4627972cf7fc58575638bffe497e4fd3fb
abdul <abdul.tanveer@gmail.com>
2024-07-02 14:40:18 +0530

    texteditingcontroller - get the text from textfield

--- lib/forms/textfield_demo.dart
+++ lib/forms/textfield_demo.dart
@@ -17,14 +17,22 @@ class TextFieldApp extends StatelessWidget {
   }
 }
 
-class MyForm extends StatelessWidget {
+class MyForm extends StatefulWidget {
   const MyForm({super.key});
+
+  @override
+  State<MyForm> createState() => _MyFormState();
+}
+//what is builder designn pattern -- flat - 3M , car -2, interior-2 , 1L
+class _MyFormState extends State<MyForm> {
+  final myController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         TextField(
+          controller: myController,
           decoration: InputDecoration(
             border: OutlineInputBorder(),
             hintText: "enter your name"
@@ -36,7 +44,15 @@ class MyForm extends StatelessWidget {
             border: UnderlineInputBorder(),
             labelText: "enter phone no"
           ),),
-        )
+        ),
+        ElevatedButton(onPressed: (){
+          showDialog(context: context, builder: (context){
+            return AlertDialog(
+              title: Text("showing"),
+              content: Text(myController.text),
+            );
+          });
+        }, child: Text('show content'))
       ],
     );
   }
