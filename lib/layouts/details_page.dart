6dc53832695832f8ebdc27977c25940c08dc1fe8
abdul <abdul.tanveer@gmail.com>
2024-07-02 10:12:12 +0530

    layouts-2

--- lib/layouts/details_page.dart
+++ lib/layouts/details_page.dart
@@ -8,7 +8,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: "layouts demo",
+      home: Scaffold(
+        appBar: AppBar(title: Text("Destination details"),),
+        body: Column(
+          children: [
+            Text("image row 1"),
+            Text("title row 1"),
+            Text("buttons row 1"),
+            Text(" description row 1"),
+
+          ],
+        ),
+      ),
+    );
     //image
     //title section
     //buttons section
