f8513bae6abc8c05496a3cdd60f094f2651d0c4d
abdul <abdul.tanveer@gmail.com>
2024-07-02 10:31:42 +0530

    column-cross axis alignment

--- lib/layouts/widgets/title.dart
+++ lib/layouts/widgets/title.dart
@@ -12,10 +12,14 @@ class TitleSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //rows' mainn axis is x-axis and cross axis is y axis
       return Row(
+
         children: [
           Expanded(
             child: Column(
+              //columns' mainn axis is y-axis and cross axis is x axis
+           crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text('$name'),
               Text('$location')
