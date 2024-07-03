d6280228665145d39aca4b9e34a6f16adb8bfdd9
abdul <abdul.tanveer@gmail.com>
2024-07-03 10:06:32 +0530

    lists

---
+++ lib/lists/simple_list.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+//as much as possible try to create stateless widgets,
+//variables use final vars
+//const connstructors
+
+void main() => runApp(ListApp());
+
+// tree  shaking in flutter -- winter leaves ffall-- state changes -- wigets need to be dropped and new ones to be create
+
+class ListApp extends StatelessWidget {
+  const ListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    title: "lists demo",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("lists demo"),
+        ),
+        body: ListView(
+         // scrollDirection: Axis.horizontal,
+          children: const <Widget>[
+            ListTile(leading: Icon(Icons.photo_album),title: Text("album")),
+            ListTile(leading: Icon(Icons.school),title: Text("school"),),
+            ListTile(leading: Icon(Icons.car_rental),title: Text("car"),),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -63,4 +63,5 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
+
 }
