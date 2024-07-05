419ef2d195e490dd53efd4f56204c0ceef3bb2e2
abdul <abdul.tanveer@gmail.com>
2024-07-05 10:28:40 +0530

    mockito

--- test/counter_test.dart
+++ test/counter_test.dart
@@ -3,10 +3,13 @@ import 'package:siemens/counter_app.dart';
 
 void main(){
   test('Counter value should be increment',(){
+
+   // group("testing increment", (){}
     final counter = Counter();
     counter.increment();
     int actual = counter.value;
     int expected = 2;
     expect(actual, expected);
   });
+
 }
